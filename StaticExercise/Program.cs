494aca8b00057394788ca85276befcab14ab8ba8
dewan <dewan@DEWANSPC>
2022-11-08 17:44:04 -0500

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using StaticExercise;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is the temp in F you want to convert?");
+        var fahtemp = Convert.ToDouble(Console.ReadLine());
+        double convertToCel = TempConverter.FahrenheitToCelcius(fahtemp);
+
+        Console.WriteLine($"That equals {convertToCel} degrees Celsius");
+
+        Console.WriteLine("What is the temp in C you want to convert?");
+        var CelTemp = Convert.ToDouble(Console.ReadLine());
+        double convertTofah = TempConverter.CelsiusToFahrenheit(CelTemp);
+
+        Console.WriteLine($"That equals to {convertTofah} degrees fahrenheit");
+    }
+}
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double ftemp)
+        {
+            var celTemp = (ftemp - 32) * (5 / 9);
+            return celTemp;
+        }
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            var fahTemp = (cTemp * (9 / 5)) + 32;
+            return fahTemp;
+        }
+    }
+}
